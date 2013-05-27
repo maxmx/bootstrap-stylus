e8bbcd8cd2b0d337c55a176a67cc66dca73f506f
shaunwong <shaun.kirk.wong@gmail.com>
2013-05-27 14:48:27 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// Based on Nib.js
+
+var stylus = require('stylus')
+	, nodes = stylus.nodes
+	, utils = stylus.utils
+
+exports = module.exports = plugin;
+
 exports.path = __dirname;
+
+function plugin() {
+	return function(style){
+		style.include(__dirname);
+	}
+}
