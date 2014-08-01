1e819e6d58ad8dccb2216ed0cd5cc92d7e21c7f1
Kane <omgsif@gmail.com>
2014-08-01 11:49:05 +1000

    Added Autoprefixer
    Fixes #59
    Config taken from official Bootstrap config

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,33 @@ module.exports = function(grunt) {
     dist: ["dist/*.css", "dist/*.js"]
   },
 
+  autoprefixer: {
+    options: {
+      browsers: [
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24', // Firefox 24 is the latest ESR
+        'Explorer >= 8',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6'
+      ]
+    },
+    core: {
+      options: {
+        map: true
+      },
+      src: 'dist/bootstrap.css'
+    },
+    theme: {
+      options: {
+        map: true
+      },
+      src: 'dist/theme.css'
+    }
+  },
+
   cssmin: {
     bootstrap: {
       files: {
